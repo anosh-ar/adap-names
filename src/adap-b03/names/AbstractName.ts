94409fcaada4000621af5daff181be999439ab20
anosh-ar <ahmadanosh.ar@gmail.com>
2025-11-24 13:07:54 +0100

    Homework B03 implementation

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -6,52 +6,113 @@ export abstract class AbstractName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter;
     }
 
+    // @methodtype conversion-method
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        return this.doClone();
     }
 
+    protected abstract doClone(): Name;
+
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const components: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            components.push(this.unmaskComponent(this.getComponent(i)));
+        }
+        return components.join(delimiter);
     }
 
+    // @methodtype conversion-method
     public toString(): string {
         return this.asDataString();
     }
 
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const components: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            components.push(this.getComponent(i));
+        }
+        return components.join(DEFAULT_DELIMITER);
     }
 
+    // @methodtype boolean-query-method
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        if (this.getNoComponents() !== other.getNoComponents()) {
+            return false;
+        }
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            if (this.getComponent(i) !== other.getComponent(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    // @methodtype get-method
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        let hashCode: number = 0;
+        const s: string = this.asDataString();
+        for (let i: number = 0; i < s.length; i++) {
+            let c: number = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+        return hashCode;
     }
 
+    // @methodtype boolean-query-method
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() === 0;
     }
 
+    // @methodtype get-method
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
-    abstract getNoComponents(): number;
+    // @methodtype get-method
+    public abstract getNoComponents(): number;
 
-    abstract getComponent(i: number): string;
-    abstract setComponent(i: number, c: string): void;
+    // @methodtype get-method
+    public abstract getComponent(i: number): string;
 
-    abstract insert(i: number, c: string): void;
-    abstract append(c: string): void;
-    abstract remove(i: number): void;
+    // @methodtype set-method
+    public abstract setComponent(i: number, c: string): void;
 
+    // @methodtype command-method
+    public abstract insert(i: number, c: string): void;
+
+    // @methodtype command-method
+    public abstract append(c: string): void;
+
+    // @methodtype command-method
+    public abstract remove(i: number): void;
+
+    // @methodtype command-method
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
+    }
+
+    // @methodtype helper-method
+    protected unmaskComponent(component: string): string {
+        let result = '';
+        let i = 0;
+        while (i < component.length) {
+            if (component[i] === ESCAPE_CHARACTER && i + 1 < component.length) {
+                result += component[i + 1];
+                i += 2;
+            } else {
+                result += component[i];
+                i++;
+            }
+        }
+        return result;
     }
 
 }
--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -8,64 +8,106 @@ export class StringName extends AbstractName {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
+        super(delimiter);
+        this.name = source;
+        this.noComponents = this.calculateNoComponents();
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
+    // @methodtype conversion-method
+    protected doClone(): Name {
+        return new StringName(this.name, this.delimiter);
     }
 
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        if (i < 0 || i >= this.noComponents) {
+            throw new Error("Index out of bounds");
+        }
+        const components = this.getComponents();
+        return components[i];
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype set-method
+    public setComponent(i: number, c: string): void {
+        if (i < 0 || i >= this.noComponents) {
+            throw new Error("Index out of bounds");
+        }
+        const components = this.getComponents();
+        components[i] = c;
+        this.name = components.join(this.delimiter);
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public insert(i: number, c: string): void {
+        if (i < 0 || i > this.noComponents) {
+            throw new Error("Index out of bounds");
+        }
+        const components = this.getComponents();
+        components.splice(i, 0, c);
+        this.name = components.join(this.delimiter);
+        this.noComponents++;
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public append(c: string): void {
+        if (this.isEmpty()) {
+            this.name = c;
+        } else {
+            this.name += this.delimiter + c;
+        }
+        this.noComponents++;
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public remove(i: number): void {
+        if (i < 0 || i >= this.noComponents) {
+            throw new Error("Index out of bounds");
+        }
+        const components = this.getComponents();
+        components.splice(i, 1);
+        this.name = components.join(this.delimiter);
+        this.noComponents--;
     }
 
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+    // @methodtype helper-method
+    protected getComponents(): string[] {
+        if (this.name === "") {
+            return [];
+        }
+
+        const components: string[] = [];
+        let currentComponent = '';
+        let i = 0;
+
+        while (i < this.name.length) {
+            if (this.name[i] === ESCAPE_CHARACTER && i + 1 < this.name.length) {
+                currentComponent += this.name[i] + this.name[i + 1];
+                i += 2;
+            } else if (this.name[i] === this.delimiter) {
+                components.push(currentComponent);
+                currentComponent = '';
+                i++;
+            } else {
+                currentComponent += this.name[i];
+                i++;
+            }
+        }
+        
+        components.push(currentComponent);
+        return components;
+    }
+
+    // @methodtype helper-method
+    protected calculateNoComponents(): number {
+        if (this.name === "") {
+            return 0;
+        }
+        return this.getComponents().length;
     }
 
 }
